87fa498d482ec0dad4b654ab7f5af335943111f8
Allen Keng <allenkeng8@gmail.com>
2024-04-18 15:11:40 -0700

    Added video, main code, and updated changelog.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,14 +1,14 @@
 # CHANGELOG
 
-## {DATE}
+## APril 18, 2024
 ### Contributors
-...
+* Allen Keng
 
 ### Added
-...
+* submission.mp4 : short video displaying VUMeter picking up sound, displaying max, mid, and low. 
 
 ### Changed
-...
+None
 
 ### Removed
-...
+None
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import board
 from digitalio import DigitalInOut, Direction
 from analogio import AnalogIn
 from time import sleep
+import math
 
 # setup pins
 microphone = AnalogIn(board.IO1)
@@ -13,9 +14,29 @@ led_pins = [
     board.IO21,
     board.IO26, # type: ignore
     board.IO47,
-    # do the rest...
+    board.IO33, 
+    board.IO34, 
+    board.IO48,
+    board.IO35,
+    board.IO36,
+    board.IO37,
+    board.IO38,
+    board.IO39
 ]
 
+threshold = [
+    24000,
+    25181.82,
+    26363.64,
+    28545.46,
+    29727.28,
+    30909.1,
+    32090.92,
+    34272.74,
+    36454.56,
+    38636.38,
+    45818.2
+    ]
 leds = [DigitalInOut(pin) for pin in led_pins]
 
 for led in leds:
@@ -25,12 +46,21 @@ for led in leds:
 while True:
     volume = microphone.value
 
-    print(volume)
+     # Check each threshold and turn on LED if volume exceeds threshold
+    for i, thresh in enumerate(threshold):
+        print(volume)
+        if volume > thresh:
+            leds[i].value = True
+        else:
+            leds[i].value = False
 
-    leds[0].value = not leds[0].value
-    leds[1].value = not leds[0].value
+    sleep(0.15)  # Adjust sleep time as needed
 
-    sleep(1)
+    # max 48000
+    # min 24000
+
+    #leds[0].value = not leds[0].value
+    #leds[1].value = not leds[0].value
 
     # instead of blinking,
     # how can you make the LEDs
